9687e55e58d2814450d2756698e6f37beec45afa
ARJ0 <amuljivani1999@gmail.com>
2024-04-10 01:08:09 -0400

    api

--- src/API.js
+++ src/API.js
@@ -1,2 +1,6 @@
-export const API = "http://localhost:8081/api"
-export const host = "http://localhost:8081";
+// export const API = "http://localhost:8081/api"
+// export const host = "http://localhost:8081";
+
+export const API = "https://rentit-backend-yln4.onrender.com/api"
+export const host = "https://rentit-backend-yln4.onrender.com";
+
